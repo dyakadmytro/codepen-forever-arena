46cec1f9123284036c652bbf33218f0a72fba2fb
Dmitriy <neposydyakadmytro@gmail.com>
2023-11-25 21:09:24 +0200

    Replace BattlePage sculls by canvas paths

--- src/pages/BattlePage/BattlePage.tsx
+++ src/pages/BattlePage/BattlePage.tsx
@@ -5,6 +5,8 @@ import PlayerHeroBattleContainer from "../../components/PlayerHeroBattleContaine
 import EnemyHeroBattleContainer from "../../components/EnemyHeroBattleContainer/EnemyHeroBattleContainer";
 import {Fighter} from "../../core/Figter";
 import GameLog from "../../components/GameLog/GameLog";
+import MoleSkull from "../../components/MoleSkull/MoleSkull";
+import {Layer, Stage} from "react-konva";
 
 
 /*TODO
@@ -15,24 +17,29 @@ import GameLog from "../../components/GameLog/GameLog";
 * */
 
 
-const RADIUS = 90;
+const RADIUS = 80;
 const DISPLAY_DURATION = 3000;
-const SVG_PATH = '/assets/images/1746206.svg';
 
 enum GameStatus {
     GAME_START = 'game_start',
     PREP_START = 'prep_start',
-    PREP_END = 'prep_start',
     TURN_START = 'turn_start',
     TURN_END = 'turn_end',
     GAME_END = 'game_end'
 }
+type ScullData = {
+    id: string,
+    key: string,
+    x: number,
+    y: number,
+    rt: number,
+    dr: number
+}
 
 const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, enemy: Fighter}) => {
-    const canvasRef = useRef(null);
-    const readyCircleRef = useRef(null);
-    const timerRef = useRef(null);
-    const rollingSkullRef = useRef(null);
+    const readyCircleRef = useRef<any>(null);
+    const timerRef = useRef<any>(null);
+    const rollingSkullRef = useRef<any>(null);
     const [accuracy, setAccuracy] = useState(0);
     const [log, setLog] = useState<any[]>([]);
     const [playerHealthPercent, setPlayerHealthPercent] = useState(100);
@@ -42,12 +49,11 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
     const [winner, setWinner] = useState<Fighter|null>(null);
     const [popupMessage, setPopupMessage] = useState(0);
     const [enemyDamage, setEnemyDamage] = useState(0);
-    const [gameStatus, setGameStatus] = useState(GameStatus.GAME_START);
+    const [gameStatus, setGameStatus] = useState<string>(GameStatus.GAME_START);
     const [tm, setTM] = useState(false);
     const [tmRolling, setTMRolling] = useState(false);
     const [skulls, setSkulls] = useState([]);
-    const [taps, setTaps] = useState([]);
-    const tapsRef = useRef(taps);
+    const [taps, setTaps] = useState<any[]>([]);
 
     useEffect(() => {
         setLog([...log, (<span>Lets BATTLE begin!</span>)])
@@ -55,12 +61,39 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
     }, []);
 
     useEffect(() => {
+        //@ts-ignore
+        if(gameStatus == GameStatus.TURN_START) setTM(setTimeout(turnEnd, DISPLAY_DURATION))
+        if(gameStatus == GameStatus.TURN_END) result()
+    }, [gameStatus]);
+
+    useEffect(() => {
+        // console.log(taps)
+        // console.log(gameStatus)
+        if(taps.length >= skulls.length && gameStatus == GameStatus.TURN_START) {
+            //@ts-ignore
+            clearTimeout(tm)
+            turnEnd()
+        }
+    }, [taps]);
+
+
+    useEffect(() => {
+        const tt = mapHitpointsToPercents(player.health - playerDamage, player.health, 100);
+        setPlayerHealthPercent(tt)
+    }, [playerDamage]);
+
+    useEffect(() => {
+        const tt = mapHitpointsToPercents(enemy.health - enemyDamage, enemy.health, 100);
+        setEnemyHealthPercent(tt)
+    }, [enemyDamage]);
+
+
+    useEffect(() => {
         if (playerDamage >= player.health){
             setLog([...log, (<p>*** <strong>{enemy.name}</strong> WINs *** <br/> <span>you defeated</span> </p>)])
             setTimeout(() => {
                 firePopUp((<div><p>! You defeated !</p> <p>LOOSER</p></div>))
             }, 2000)
-
             setWinner(enemy)
         } else if (enemyDamage >= enemy.health) {
             setLog([...log, (<p>*** <strong>{player.name}</strong> WINs *** <br/> <span>Congratulations!!</span> </p>)])
@@ -70,10 +103,6 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
             setWinner(player)
         }
     }, [playerDamage, enemyDamage]);
-
-    useEffect(() => {
-        if (accuracy > 0) processHit()
-    }, [accuracy])
 
     function firePopUp(message: any) {
         setPopupMessage(message);
@@ -86,168 +115,121 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
 
     function rollingSkull() {
         return new Promise(resolve => {
-            //@ts-ignore
             rollingSkullRef.current.classList.add('rolling-skull')
-            //@ts-ignore
             rollingSkullRef.current.style.display = 'block'
-            //@ts-ignore
             rollingSkullRef.current.addEventListener('animationend', function() {
                 rollingSkullStop()
             });
             setTimeout(() => {
                 //@ts-ignore
                 resolve();
-            }, 2000); // 2000 milliseconds = 2 seconds
+            }, 2000);
         });
     }
 
     function rollingSkullStop() {
-        //@ts-ignore
         rollingSkullRef.current.style.display = 'none';
-        //@ts-ignore
         rollingSkullRef.current.classList.remove('rolling-skull');
     }
 
-    useEffect(() => {
-        //@ts-ignore
-        if(gameStatus == GameStatus.TURN_START) setTM(setTimeout(turnEnd, DISPLAY_DURATION))
-    }, [gameStatus]);
+    function doElementsIntersect(scull1: ScullData, scull2: ScullData) {
+        return makeRect(scull1).intersects(makeRect(scull2), 1);
+    }
 
-    useEffect(() => {
-        tapsRef.current = taps;
-        if(taps.length >= skulls.length) {
-            //@ts-ignore
-            clearTimeout(tm)
-            turnEnd()
-        }
-    }, [taps]);
-
-    useEffect(() => {
-        const tt = mapHitpointsToPercents(player.health - playerDamage, player.health, 100);
-        setPlayerHealthPercent(tt)
-    }, [playerDamage]);
-
-    useEffect(() => {
-        const tt = mapHitpointsToPercents(enemy.health - enemyDamage, enemy.health, 100);
-        setEnemyHealthPercent(tt)
-    }, [enemyDamage]);
-
-    function makeRect(img: any) {
-        const topLeft1 =  new paper.Point(parseInt(img.props.style.left), parseInt(img.props.style.top));
+    function makeRect(scull: ScullData) {
+        const topLeft1 =  new paper.Point(scull.x - 35, scull.y - 35);
         const rectSize1 = new paper.Size(RADIUS, RADIUS);
         return new paper.Rectangle(topLeft1, rectSize1);
     }
 
-    function doElementsIntersect(img1: any, img2: any) {
-         return makeRect(img1).intersects(makeRect(img2), 1);
+    function generateRandomSkull( xMax:  number, yMax: number): ScullData {
+        const id = Math.random() * 100
+        const x = getRandomBetween(RADIUS, xMax)
+        const y = getRandomBetween(RADIUS, yMax)
+        const rt = Math.round(Math.random() * 360)
+        const ms = Math.random() * 0.9 + 0.1;
+
+        return {
+            id: id + '',
+            key: id + '',
+            x: x,
+            y: y,
+            rt: rt,
+            dr: ms
+        }
     }
 
-    function generateRandomSkull(index: string, xMax:  number, yMax: number) {
-        const x = Math.random() * xMax;
-        const y = Math.random() * yMax;
-        const rt = Math.random() * 360
-        const ms = Math.random() * 500
-        return (
-            <img
-                key={index}
-                id={index}
-                src={SVG_PATH}
-                style={{
-                    position: 'absolute',
-                    top: `${y}px`,
-                    left: `${x}px`,
-                    transform: `rotate(${rt}deg)`,
-                    animation: `fadeIn ${ms}ms ease-in`
-                }}
-            />
-        );
-    }
-
-    function generateSkulls(amount: number) {
+    function generateSkulls(amount: number): any {
         const newSkulls: any[] = [];
         do {
-            const id = Math.random() * 100;
-            const img = generateRandomSkull(id + '', 420, 270)
+            const img = generateRandomSkull(420, 270)
             newSkulls.push(img)
         } while (newSkulls.every((skull, index) => {
-                newSkulls.every((s: any) => {
-                    if (s == skull) return false
-                    if(doElementsIntersect(s, skull)) newSkulls.splice(index,1)
-                    return true
-                });
-                return true;
-            }) && newSkulls.length < amount )
-
-        setGameStatus(GameStatus.TURN_START)
-        setTaps([]);
-        //@ts-ignore
-        setSkulls(newSkulls);
+            newSkulls.every((s: any) => {
+                if (s == skull) return false
+                if(doElementsIntersect(s, skull)) newSkulls.splice(index,1)
+                return true
+            });
+            return true;
+        }) && newSkulls.length < amount )
         return newSkulls;
     }
 
     function turnStart() {
-        if(gameStatus == GameStatus.PREP_END) return
-
+        if(gameStatus == GameStatus.TURN_START) return
+        setTaps([])
+        setAccuracy(0)
         setGameStatus(GameStatus.TURN_START)
+        setSkulls(generateSkulls(5))
 
-        generateSkulls(5)
-
-        //@ts-ignore
         timerRef.current.style.display = 'block'
-        //@ts-ignore
         timerRef.current.style.animation = `${DISPLAY_DURATION}ms spin linear`
         //@ts-ignore
         document.getElementById('bg-skeleton-9').style.animation = `${DISPLAY_DURATION}ms spinBack backwards`
     }
 
     function turnEnd() {
-        if(gameStatus !== GameStatus.TURN_START) return
-        setGameStatus(GameStatus.TURN_END);
-        result()
+        // if(gameStatus !== GameStatus.TURN_START) return
+        setGameStatus(GameStatus.TURN_END)
     }
 
     function result() {
-        //@ts-ignore
         timerRef.current.style.animation = '';
-        //@ts-ignore
         timerRef.current.addEventListener('animationend', function() {
-            //@ts-ignore
             timerRef.current.style.display = 'none';
         });
-        //@ts-ignore
         timerRef.current.style.animation = 'fadeOut 1s ease-in';
         //@ts-ignore
         document.getElementById('bg-skeleton-9').style.animation = ''
-        const averageAccuracy = tapsRef.current.reduce((a, b) => a + b, 0) / skulls.length;
-        //@ts-ignore
-        setAccuracy(parseFloat(averageAccuracy.toFixed(2)));
-        // rollingSkull()
+        const averageAccuracy = Math.round(taps.reduce((a, b) => a + b, 0) / skulls.length);
+        setAccuracy(averageAccuracy);
+        processHit(averageAccuracy)
     }
 
     function hitChance(agility: number, accuracy: number) {
-        return ((accuracy * agility) + 10) / Math.random() * 100
+        return (accuracy * agility) / 100
     }
 
     function doHit(aggressor: Fighter, victim: Fighter, aggressorAccuracy: number, victimAccuracy: number) {
-        const effectivePower = aggressor.power * (1 + aggressorAccuracy / 100);
+        const effectivePower = Math.round(aggressor.power * (1 + aggressorAccuracy / 70));
         const effectiveDefense = victim.defence
-
-        let damage = Math.round(effectivePower - effectiveDefense);
+        const damage = Math.round(effectivePower - effectiveDefense);
         return Math.max(0, damage);
     }
 
-    function processHit() {
+    function processHit(accuracy: number) {
         const enemyAccuracy = getRandomBetween(50, 90)
         const playerChance = hitChance(player.agility, accuracy)
         const enemyChance = hitChance(enemy.agility, enemyAccuracy)
         let logMassage = null;
+        // console.log('Chance', playerChance, enemyChance, enemyAccuracy)
         if (playerChance >= enemyChance) {
             const damage = doHit(player, enemy, accuracy, enemyAccuracy)
-            logMassage =( <span>💀 <strong>{player.name}</strong> made {damage} dmg, *accuracy <strong>{accuracy}</strong>/ against <strong>{enemyAccuracy}</strong></span> )
+            logMassage =( <span>💀 <strong>{player.name}</strong> made <strong>{damage}</strong> dmg, *accuracy <strong>{accuracy}</strong>/ against <strong>{enemyAccuracy}</strong></span> )
             setEnemyDamage(enemyDamage + damage)
         } else {
             const damage = doHit(enemy, player, enemyAccuracy, accuracy)
-            logMassage =(<span>💀 <strong>{enemy.name}</strong> made <strong>{damage}</strong>dmg, *accuracy <strong>{enemyAccuracy}</strong>/ against <strong>{accuracy}</strong></span>)
+            logMassage =(<span>💀 <strong>{enemy.name}</strong> made <strong>{damage}</strong> dmg, *accuracy <strong>{enemyAccuracy}</strong>/ against <strong>{accuracy}</strong></span>)
             setPlayerDamage(playerDamage + damage)
         }
         setLog([...log, logMassage])
@@ -288,47 +270,35 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
         })
     }
 
-    function handleImageClick(e: any) {
-        if(gameStatus != GameStatus.TURN_START) return
-        if (e.target.id == 'gameCanvas'){
-            //@ts-ignore
-            setTaps( [...taps, 0]);
-            return;
-        }
-
-        const rect = e.target.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+    function calculateElementAccuracy(e: any) {
+        const rect = e.path.getClientRect();
+        const x = e.pointer.x - rect.x;
+        const y = e.pointer.y - rect.y;
         const center = new paper.Point(rect.width / 2, rect.height / 2)
         const click = new paper.Point(x, y)
         const distance = center.getDistance(click);
-        const accuracy = 100 - distance;
-        //@ts-ignore
-        setTaps( [...taps, accuracy]);
+        return 100 - distance
+    }
 
-        e.target.style.animation = '';
-        e.target.classList.add('cracked-skull')
-        e.target.addEventListener('animationend', function() {
-            e.target.style.pointerEvents = 'none';
-            e.target.style.opacity = 0;
-        });
+    function pushAccuracy(accuracy: number) {
+        setTaps( [...taps, accuracy]);
+    }
+
+    function handleFieldClick(e:any) {
+        if(gameStatus !== GameStatus.TURN_START) return
+        if(e.target?.nodeType == 'Stage') pushAccuracy(0)
+        if(!e.target) pushAccuracy(calculateElementAccuracy(e))
     }
 
     function handleReadyClick() {
-        //@ts-ignore
         if(gameStatus !== GameStatus.GAME_START && gameStatus !== GameStatus.TURN_END) return;
         setSkulls([]);
-
-        //@ts-ignore
         readyCircleRef.current.style.animation = 'clickBounce .5s ease-out';
-        //@ts-ignore
         readyCircleRef.current.addEventListener('animationend', function () {
-            //@ts-ignore
             readyCircleRef.current.style.animation = '';
         })
         setGameStatus(GameStatus.PREP_START)
         rollingSkull().then(() => {
-            setGameStatus(GameStatus.PREP_END)
             turnStart()
         })
     }
@@ -348,15 +318,26 @@ const BattlePage = ({ toRoute, player, enemy }: {toRoute: any, player: Fighter, 
                 <div ref={rollingSkullRef} style={{display: "none"}}>
                     <img src="/assets/images/1531576.svg"/>
                 </div>
-                <div ref={canvasRef} id="gameCanvas" onClick={handleImageClick}>
-                    {skulls.map(skull => skull)}
-                </div>
+                <Stage id="gameCanvas" key={12} width={500} height={350} onClick={handleFieldClick}>
+                    <Layer >
+                        {skulls.map((skull:any) => {
+                            return <MoleSkull data={{
+                                id: skull.id,
+                                key: skull.id,
+                                x: skull.x,
+                                y: skull.y,
+                                rt: skull.rt,
+                                duration: skull.dr,
+                                handleImageClick: handleFieldClick
+                            }} />
+                        })}
+                    </Layer>
+                </Stage>
             </div>
             <div id="accuracy-interface" >
                 <img id="accuracy-interface-img" onClick={handleAccuracyClick} src='/assets/images/2029570.png'/>
                 <p>{accuracy}</p>
             </div>
-            {/*@ts-ignore*/}
             <PlayerHeroBattleContainer health={playerHealthPercent} hero={player} />
             <EnemyHeroBattleContainer health={enemyHealthPercent} hero={enemy} />
             <GameLog log={log}/>
