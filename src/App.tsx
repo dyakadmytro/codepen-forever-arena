2c74bb00a2f01d8fff82176147720dd3e9cb8e4b
Dmitriy <neposydyakadmytro@gmail.com>
2023-11-27 23:08:04 +0200

    Chance background gradient and Heroes config balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
-import StartPage from "./pages/StartPage";
+import StartPage from "./pages/StartPage/StartPage";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import BattlePage from "./pages/BattlePage/BattlePage";
-import ResultsPage from "./pages/ResultsPage";
+import ResultsPage from "./pages/ResultsPage/ResultsPage";
 import {Fighter} from "./core/Figter";
-
 
 
 export type battlePageData = {
@@ -14,7 +13,7 @@ export type battlePageData = {
 }
 function App() {
   const [step, setStep] = useState('start');
-  const [data, setData] = useState({});
+  const [data, setData] = useState<any>({});
 
   const toRoute = (step = 'start', data = {}) => {
     setStep(step);
@@ -23,11 +22,10 @@ function App() {
 
   return (
     <div className="App">
-        {step === 'start' && (<StartPage toRoute={toRoute} />)}
-        {step === 'menu' && (<MenuPage toRoute={toRoute} />)}
-      {/*@ts-ignore*/}
-        {step === 'battle' && (<BattlePage toRoute={toRoute} player={data.player} enemy={data.enemy}/>)}
-        {step === 'results' && (<ResultsPage toRoute={toRoute} />)}
+        {step === 'start' && (<StartPage toRoute={toRoute} key='StartPage'/>)}
+        {step === 'menu' && (<MenuPage toRoute={toRoute} key='MenuPage'/>)}
+        {step === 'battle' && (<BattlePage toRoute={toRoute} player={data.player} enemy={data.enemy} key='BattlePage'/>)}
+        {step === 'results' && (<ResultsPage toRoute={toRoute} fighter={data.fighter} key='ResultsPage'/>)}
     </div>
   );
 }
