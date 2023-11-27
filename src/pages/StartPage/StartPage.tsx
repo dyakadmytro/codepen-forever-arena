98cf3b64a5d19ea00307d14f218ab0206567eeaf
Dmitriy <neposydyakadmytro@gmail.com>
2023-11-27 23:09:31 +0200

    Remove claw header

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react";
-import ClawHeader from "../components/ClawHeader";
 
 const StartPage = ({ toRoute }: { toRoute: any }) => {
 
@@ -11,7 +10,6 @@ const StartPage = ({ toRoute }: { toRoute: any }) => {
     return (
         <div className="page">
             <img id="bg-skeleton-2" src="/assets/images/2968614.svg"/>
-            <ClawHeader />
             <h1 className='welcome-text'>!Welcome to the <span>Cracked Bones</span>!</h1>
             <div className="start-container">
                 <div className="start-button" onClick={handleStartClick}>Start</div>
